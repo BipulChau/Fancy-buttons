0b7b9a80dad8a6d9687b9677dc60c0b1bea34b8d
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-25 14:36:09 -0400

    Reading from the state-repetitions in TextRepeaterButton component

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 
 function TextRepeaterButton(props) {
   const [repetitions, setRepetitions] = useState();
+  const textArray = [];
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key = {i}>I love our Oggy!!!</span>)
+  }
   return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+      {textArray}
     </button>
   );
 }
