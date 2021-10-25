458d1f13fca4d3b94b9df139675f64512cda6911
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-25 12:44:34 -0400

    created AngryButton component

---
+++ src/components/AngryButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AngryButton() {
+  return (
+    <button className="AngryButton"> 
+    {/*If you have not reached the maximum */} <span>Don't click me too much! </span>
+    {/*If you have reached the maximun */} <span>Rawr! </span>
+     </button>
+  )
+}
+
+export default AngryButton;
