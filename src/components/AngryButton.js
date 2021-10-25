be397e3afc854ba055f8aaca4bded2169b74c3f3
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-25 14:18:36 -0400

    Reading from the state in AngryButton component

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -3,10 +3,13 @@ import React, { useState } from "react";
 function AngryButton(props) {
   const [anger, setAnger] = useState();
   return (
-    <button className="AngryButton">
-      {/*If you have not reached the maximum */}{" "}
-      <span>Don't click me too much! </span>
-      {/*If you have reached the maximun */} <span>Rawr! </span>
+    <button
+      style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }}
+      className="AngryButton"
+    >
+      {/* ${anger} is the opacity decimal. This value can be anything from 0.0 to 1.0.*/}
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr! </span>}
     </button>
   );
 }
