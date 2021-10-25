1b8274339ad85fe08c702ce591a1e0be39ac85a8
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-25 14:27:38 -0400

    Reading from the state-light in LightSwitchButton component

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -7,7 +7,6 @@ function AngryButton(props) {
       style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }}
       className="AngryButton"
     >
-      {/* ${anger} is the opacity decimal. This value can be anything from 0.0 to 1.0.*/}
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr! </span>}
     </button>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -4,14 +4,8 @@ function LightSwitchButton(props) {
   const [light, setLight] = useState();
   return (
     <button className="LightSwitchButton">
-      {/* When the state is on */}{" "}
-      <span className="on">
-        <i>💡</i> I'm on on!
-      </span>
-      {/* When the state is off */}{" "}
-      <span className="off">
-        <i>💡</i> I'm on off!
-      </span>
+      {light === "on" &&  <span className="on"><i>💡</i> I'm on!</span>}
+      {light === "off" &&  <span className="off"><i>💡</i> I'm on off!</span>}
     </button>
   );
 }
